7cee7c2ed8f3f652c69113548579d79816aa378b
daemotron <mail@daemotron.net>
2023-05-14 12:51:23 +0200

    Add 128 bit int type

---
+++ include/xppl_int.h
@@ -0,0 +1,21 @@
+#ifndef _XPPL_INT_H_
+#define _XPPL_INT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#ifdef __SIZEOF_INT128__
+#define XPPL_INT_128
+typedef __int128_t int128_t;
+typedef __uint128_t uint128_t;
+#endif
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif /* _XPPL_INT_H_ */
+
