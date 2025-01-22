e06cef36adceba71818c1fcdb676a6a4ec560415
daemotron <mail@daemotron.net>
2025-01-22 08:53:41 +0100

    macros for socket shutdown

--- include/xppl_socket.h
+++ include/xppl_socket.h
@@ -8,9 +8,15 @@ extern "C" {
 #if IBM
 typedef unsigned long long xppl_socket_t;
 #define XPPL_SOCKET_FAILED ~0ULL
+#define xppl_socket_shutdown_receive(fd) shutdown(fd, SD_RECEIVE)
+#define xppl_socket_shutdown_send(fd) shutdown(fd, SD_SEND)
+#define xppl_socket_shutdown_both(fd) shutdown(fd, SD_BOTH)
 #else /* !IBM */
 typedef int xppl_socket_t;
 #define XPPL_SOCKET_FAILED -1
+#define xppl_socket_shutdown_receive(fd) shutdown(fd, SHUT_RD)
+#define xppl_socket_shutdown_send(fd) shutdown(fd, SHUT_WR)
+#define xppl_socket_shutdown_both(fd) shutdown(fd, SHUT_RDWR)
 #endif
 
 void xppl_socket_init(void);
